4fb778bc1f559688d456a4be4133a0154b89497c
Kushal Garg <kushalgarg2000@gmail.com>
2020-09-30 13:12:04 +0530

    Removes peerjs

--- public/script.js
+++ public/script.js
@@ -1,52 +1,22 @@
 const socket = io.connect("http://localhost:3000");
 let myVideoStream;
-const myvideo = document.createElement('video');
+const myvideo = document.createElement("video");
 myvideo.muted = true;
 
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then(stream => {
+		myVideoStream = stream;
+		addvideoStream(myvideo, stream);
+	});
 
-var peer = new Peer(undefined,{
-    path:'/peerjs',
-    host:'/',
-    port:'3000'
-});
-
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream =>{
-    myVideoStream = stream;
-    addvideoStream(myvideo,stream)
-
-    peer.on('call', call=>{
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream=>{
-            addvideoStream(video,userVideoStream)
-        })
-    })
-    socket.on("user-connected",(userId)=>{
-        connectToUser(userId,stream);
-    });
-});
-
-
-peer.on('open',id=>{
-    socket.emit("join-room", ROOMID, id);
-});
-
-
-const connectToUser = (userId,stream)=>{
-    const call = peer.call(userId,stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream=>{
-        addvideoStream(video,userVideoStream)
-    });
-}
-
-const addvideoStream = (video,stream)=>{
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", function(){
-        video.play();
-    });
-    document.getElementById("video-grid").append(video);
-}
+const addvideoStream = (video, stream) => {
+	video.srcObject = stream;
+	video.addEventListener("loadedmetadata", function () {
+		video.play();
+	});
+	document.getElementById("video-grid").append(video);
+};
