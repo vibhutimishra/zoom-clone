3695bc30d69b5e99bc7dc5898be39537ecd19e89
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-21 22:00:36 +0530

    Final

--- public/script.js
+++ public/script.js
@@ -1,5 +1,19 @@
 const socket = io.connect("http://localhost:3000");
-socket.emit("join-room", ROOMID);
+
+const myPeer = new Peer(undefined,{
+	host:"/",
+	port:"3001"
+});
+
+myPeer.on('open', id=>{
+	socket.emit("join-room", ROOMID, id);
+});
+
+socket.on('user-connected', userId =>{
+	console.log("user connected "+userId);
+});
+
+const videoGrid = document.getElementById('video-grid');
 let myVideoStream;
 const myvideo = document.createElement("video");
 myvideo.muted = true;
@@ -12,12 +26,35 @@ navigator.mediaDevices
 	.then(stream => {
 		myVideoStream = stream;
 		addvideoStream(myvideo, stream);
+
+		myPeer.on("call",call=>{
+			call.answer(stream)
+			const video = document.createElement("video")
+			call.on('stream',userVideoStream=>{
+				addvideoStream(video,userId)
+			})
+		})
+
+		socket.on("user-connected", userId=>{
+			connectToNewUser(userId,stream)
+		});
 	});
 
-const addvideoStream = (video, stream) => {
+function connectToNewUser(userId,stream){
+	const call= myPeer.call(userId,stream)
+	const video = document.createElement('video')
+	call.on("stream",userVideoStream=>{
+		addvideoStream(video,userVideoStream)
+	})
+	call.on('close',()=>{
+		video.remove()
+	})
+}
+
+function addvideoStream(video, stream){
 	video.srcObject = stream;
-	video.addEventListener("loadedmetadata", function () {
+	video.addEventListener("loadedmetadata", ()=>{
 		video.play();
 	});
-	document.getElementById("video-grid").append(video);
+	videoGrid.append(video);
 };
