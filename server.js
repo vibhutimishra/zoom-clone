d097040e9a504157b427485232f357dceb2ad74f
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-21 22:48:29 +0530

    final

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server)
 var uuid = require("uuid");
+
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/",function(req,res){
 	var b = uuid.v4();
 	res.status(200).redirect("/" + b);
+
+});
+app.get("/:room",function(req,res){
+	res.render('room.ejs',{roomId: req.params.room})
 });
 
-app.get("/:room", function (req, res) {
-	res.render("room.ejs", { roomId: req.params.room });
-});
-
-const server = app.listen(3000, function (req, res) {
-	console.log("Server Started");
-});
-const io = require("socket.io").listen(server);
 io.on("connection", socket => {
-	socket.on("join-room", (roomId) => {
-		console.log(roomId);
+	socket.on("join-room", (roomId, userId) => {
+		console.log(roomId,userId);
 		socket.join(roomId);
+		socket.to(roomId).broadcast.emit('user-connected',userId)
 	});
 });
+
+
+server.listen(3000)
